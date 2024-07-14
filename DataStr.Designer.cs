52a388464ef6267215d5804132fff5a4cbf7ff2d
Aggeliki Apostolou <61296853+apostolouagg@users.noreply.github.com>
2024-07-15 01:18:40 +0300

    hehe
    xoxoxoxox

--- DataStr.Designer.cs
+++ DataStr.Designer.cs
@@ -28,8 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DataStr));
             this.labelDataStr = new System.Windows.Forms.Label();
             this.buttonBack = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.intro_and_setup = new System.Windows.Forms.Label();
+            this.labelBasicSyntax = new System.Windows.Forms.Label();
+            this.labelRunAndDeb = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelDataStr
@@ -37,7 +47,7 @@
             this.labelDataStr.AutoSize = true;
             this.labelDataStr.Cursor = System.Windows.Forms.Cursors.Hand;
             this.labelDataStr.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDataStr.Location = new System.Drawing.Point(457, 9);
+            this.labelDataStr.Location = new System.Drawing.Point(443, 9);
             this.labelDataStr.Name = "labelDataStr";
             this.labelDataStr.Size = new System.Drawing.Size(236, 36);
             this.labelDataStr.TabIndex = 15;
@@ -55,17 +65,89 @@
             this.buttonBack.UseVisualStyleBackColor = false;
             this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
             // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(449, 114);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(33, 26);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 52;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(449, 163);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(33, 26);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 51;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(449, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 26);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 49;
+            this.pictureBox1.TabStop = false;
+            // 
+            // intro_and_setup
+            // 
+            this.intro_and_setup.AutoSize = true;
+            this.intro_and_setup.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.intro_and_setup.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.intro_and_setup.Location = new System.Drawing.Point(488, 72);
+            this.intro_and_setup.Name = "intro_and_setup";
+            this.intro_and_setup.Size = new System.Drawing.Size(165, 25);
+            this.intro_and_setup.TabIndex = 47;
+            this.intro_and_setup.Text = "Sequence Types";
+            // 
+            // labelBasicSyntax
+            // 
+            this.labelBasicSyntax.AutoSize = true;
+            this.labelBasicSyntax.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelBasicSyntax.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBasicSyntax.Location = new System.Drawing.Point(488, 115);
+            this.labelBasicSyntax.Name = "labelBasicSyntax";
+            this.labelBasicSyntax.Size = new System.Drawing.Size(162, 25);
+            this.labelBasicSyntax.TabIndex = 46;
+            this.labelBasicSyntax.Text = "Mapping Types";
+            // 
+            // labelRunAndDeb
+            // 
+            this.labelRunAndDeb.AutoSize = true;
+            this.labelRunAndDeb.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelRunAndDeb.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRunAndDeb.Location = new System.Drawing.Point(488, 163);
+            this.labelRunAndDeb.Name = "labelRunAndDeb";
+            this.labelRunAndDeb.Size = new System.Drawing.Size(105, 25);
+            this.labelRunAndDeb.TabIndex = 45;
+            this.labelRunAndDeb.Text = "Set Types";
+            // 
             // DataStr
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
             this.ClientSize = new System.Drawing.Size(1159, 687);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.intro_and_setup);
+            this.Controls.Add(this.labelBasicSyntax);
+            this.Controls.Add(this.labelRunAndDeb);
             this.Controls.Add(this.buttonBack);
             this.Controls.Add(this.labelDataStr);
             this.Name = "DataStr";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Data Structures";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,5 +157,11 @@
 
         private System.Windows.Forms.Label labelDataStr;
         private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label intro_and_setup;
+        private System.Windows.Forms.Label labelBasicSyntax;
+        private System.Windows.Forms.Label labelRunAndDeb;
     }
 }
