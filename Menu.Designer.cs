1ad105eb12a053e976702b9810f51ebf3d489f06
apostolouagg <61296853+apostolouagg@users.noreply.github.com>
2024-07-21 18:40:52 +0300

    fehfhaihfkdhk

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -293,10 +293,10 @@
             // labelClicks
             // 
             this.labelClicks.AutoSize = true;
-            this.labelClicks.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClicks.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelClicks.Location = new System.Drawing.Point(13, 9);
             this.labelClicks.Name = "labelClicks";
-            this.labelClicks.Size = new System.Drawing.Size(79, 18);
+            this.labelClicks.Size = new System.Drawing.Size(73, 16);
             this.labelClicks.TabIndex = 47;
             this.labelClicks.Text = "labelClicks";
             // 
